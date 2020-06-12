aaa98e39b86ddc7e4e9c3ecd3e45689c3945fb38
unibreakfast <unibreakfast1@gmail.com>
2020-06-12 08:10:33 +0300

    can write by requested path

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const fs = require('fs'),  fsp = fs.promises,  http = require('http')
 
 http.createServer((req, resp)=> {
   if (req.method == 'PUT')
-    req.on('end', ()=>
+    fsp.mkdir(req.url.replace(/(^\/)|(\/[^\/]*$)/g,''), {recursive: true}).then(()=> req.on('end', ()=>
       resp.end('I hear you, here\'s a random number: '+ Math.random()))
-        .pipe(fs.createWriteStream('filename.ext'))
-  // else
+        .pipe(fs.createWriteStream(req.url.slice(1))))
+      .catch(()=> resp.end('sorry, I can\'t do that'))
+
+  else resp.end('fetch now!')
 
 }).listen(3000)
