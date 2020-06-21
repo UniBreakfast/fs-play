b7b3378b0bee649138d641a592b4310c3968a70e
unibreakfast <unibreakfast1@gmail.com>
2020-06-21 21:50:20 +0300

    support multiple files on select or drop

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 function setUploader({pathsrc, namesrc, chooser, starter, dropzone,
-                      informcb, reportcb, fetchArgs}) {
-  let file
+                      informcb, reportcb, fetchArgs, multiple}) {
+  let files
   const {body} = document,  isFn = val => typeof val=='function'
         getPath =()=> pathsrc? isFn(pathsrc)? pathsrc() : pathsrc : '',
-        getName =()=> namesrc? isFn(namesrc)? namesrc()||file.name : namesrc :
-                        file.name,
-        getArgs =()=> fetchArgs? isFn(fetchArgs)?
-                        fetchArgs(getPath(), getName(), file) :
-                          [fetchArgs[0], {...fetchArgs[1], body: file}] :
-                            defaultFetchArgs(getPath(), getName(), file),
-        start =()=> file && fetch(...getArgs()).then(reportcb || console.log)
+        getName = i => {
+          let name = namesrc? isFn(namesrc)? namesrc()||files[i].name :
+                      namesrc : files[i].name
+          if (multiple && files[1] && name!=files[i].name)
+            name = name.replace(/(\.|$)/, String(i+1).padStart(3, 0)+'$1')
+          return name
+        },
+        getArgs = i => fetchArgs? isFn(fetchArgs)?
+                        fetchArgs(getPath(), getName(i), files[i]) :
+                          [fetchArgs[0], {...fetchArgs[1], body: files[i]}] :
+                            defaultFetchArgs(getPath(), getName(i), files[i]),
+        start =()=> files?.length && files.forEach((_, i)=>
+          fetch(...getArgs(i)).then(res => reportcb(res, i) || Boolean))
 
   if (chooser) {
     const choose =()=> {
       const input = Object.assign(document.createElement('input'),
-                                  {type: 'file', hidden: true}),
+                                  {type: 'file', hidden: true, multiple}),
         work =()=> {
           body.removeEventListener('mousemove', work)
           if (input.value) {
-            file = input.files[0]
-            if (informcb) informcb(file, getPath(), getName())
+            files = [...input.files]
+            if (informcb) informcb(files, getPath(), getName)
             if (!starter) start()
           }
           input.remove()
@@ -42,8 +48,10 @@ function setUploader({pathsrc, namesrc, chooser, starter, dropzone,
   if (dropzone) {
     ['dragenter', 'dragover', 'drop'].forEach(ename => dropzone.addEventListener(ename, e => {e.stopPropagation(); e.preventDefault()}))
     dropDiv.ondrop = e => {
-      file = e.dataTransfer.files[0]
-      if (informcb) informcb(file, getPath(), getName())
+      if (![...e.dataTransfer.items].every(item =>
+        item.webkitGetAsEntry().isFile)) return
+      files = [...e.dataTransfer.files].slice(0, ...multiple? []:[1])
+      if (informcb) informcb(files, getPath(), getName)
       if (!starter) start()
     }
   }
@@ -63,9 +71,8 @@ nameInp.oninput = nameInp.onchange =()=> {
 
 setUploader({
   pathsrc: ()=> pathInp.value, namesrc: ()=> nameInp.value,
-  chooser: selectBtn,
-  starter: start => uploadBtn.onmouseover = start, dropzone: dropDiv,
-  informcb: (body, path, name)=> informer.innerHTML = `File "${body.name}" (${body.size}bytes) will be uploaded to <span id="pathSpan">${path? `"${path}"`:'root'}</span><span id="nameSpan">${name && name!=body.name? ` as "${name}"`:''}</span>`,
-  reportcb: response => response.text().then(answer => console.log("Server answered: ", answer)),
+  chooser: selectBtn, starter: uploadBtn, dropzone: dropDiv, multiple: true,
+  informcb: (files, path, getName)=> informer.innerHTML = `File(s) "${files.map(file => file.name).join(', ')}" (${(files.reduce((sum, file)=> sum+file.size, 0)/1024/1024).toFixed(2)}Mb total) will be uploaded to <span id="pathSpan">${path? `"${path}"`:'root'}</span><span id="nameSpan">${nameInp.value? ` as "${files[1]? nameInp.value.replace(/(\.|$)/, '000'+'$1') : nameInp.value}"`:''}</span>`,
+  reportcb: (response, i) => response.text().then(answer => console.log("Server answered: ", answer, i)),
   fetchArgs: (path, name, body)=> [path+'/'+name, {method: 'PUT', body}]
 })
